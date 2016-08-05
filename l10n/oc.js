d6565e30787a3554d62c5fd4efd73787e0ba6626
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-08-05 14:22:33 +0200

    [tx-robot] updated from transifex

--- l10n/oc.js
+++ l10n/oc.js
@@ -4,6 +4,7 @@ OC.L10N.register(
     "This file is too big to be opened. Please download the file instead." : "Aqueste fichièr es tròp voluminós per èsser dobèrt. Telecargatz-lo, puslèu.",
     "Cannot read the file." : "Impossible de legir lo fichièr.",
     "Invalid file path supplied." : "Camin de fichièr provesit invalid.",
+    "The file is locked." : "Lo fichièr es verrolhat.",
     "An internal server error occurred." : "Una error intèrna del servidor s'es produsida.",
     "Cannot save file as it has been modified since opening" : "Impossible de salvar lo fichièr, es estat modificat entre temps",
     "Insufficient permissions" : "Permissions insufisentas",
