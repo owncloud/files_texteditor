c5cd16bdebe2e3b60e3b43945f99cfd73fdc43fe
Phil Davis <phil@jankaritech.com>
2017-08-25 10:32:55 +0545

    Make sample UI test great again

--- tests/ui/features/bootstrap/bootstrap.php
+++ tests/ui/features/bootstrap/bootstrap.php
@@ -1,8 +1,16 @@
 <?php
 require __DIR__ . '/../../../../../../lib/composer/autoload.php';
-require __DIR__. '/../../../../../../tests/ui/features/bootstrap/BasicStructure.php';
+require __DIR__ . '/../../../../../../tests/ui/features/bootstrap/BasicStructure.php';
 
 $classLoader = new \Composer\Autoload\ClassLoader();
-$classLoader->addPsr4("Page\\", __DIR__. "/../lib", true);
-$classLoader->addPsr4("Page\\", __DIR__. "/../../../../../../tests/ui/features/lib/", true);
+$classLoader->addPsr4("Page\\", __DIR__ . "/../lib", true);
+$classLoader->addPsr4(
+	"Page\\", __DIR__ . "/../../../../../../tests/ui/features/lib/", true
+);
+$classLoader->addPsr4(
+	"TestHelpers\\", __DIR__ . "/../../../../../../tests/TestHelpers/", true
+);
+$classLoader->addPsr4(
+	"TestHelpers\\Files_TextEditor\\", __DIR__ . "/../../../TestHelpers/", true
+);
 $classLoader->register();
