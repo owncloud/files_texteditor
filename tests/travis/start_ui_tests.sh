9c5c8225ed33b30c32ede36caacfe69e9479f088
Phil Davis <phil@jankaritech.com>
2017-08-25 19:34:59 +0545

    Remove ot of date script code

--- tests/travis/start_ui_tests.sh
+++ tests/travis/start_ui_tests.sh
@@ -5,6 +5,5 @@
 # @author Phillip Davis
 # @copyright 2017 Phillip Davis phil@jankaritech.com
 #
-cp tests/ui/features/bootstrap/FeatureContext.php apps/files_texteditor/tests/ui/features/bootstrap/FeatureContext.php
 echo "Running UI tests"
 bash tests/travis/start_behat_tests.sh --config apps/files_texteditor/tests/ui/config/behat.yml
