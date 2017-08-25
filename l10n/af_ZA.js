ca7796f8256d307913ddebd0e3e62587f6503e9b
ownCloud Jenkins <jenkins@owncloud.com>
2017-08-25 02:04:20 +0000

    [tx-robot] updated from transifex

---
+++ l10n/af_ZA.js
@@ -0,0 +1,22 @@
+OC.L10N.register(
+    "files_texteditor",
+    {
+    "This file is too big to be opened. Please download the file instead." : "Dei lêer is te groot om te open. Laai dit liewer af.",
+    "Cannot read the file." : "Kan nie die lêer lees nie.",
+    "Invalid file path supplied." : "Ongeldige lêerpad verskaf.",
+    "The file is locked." : "Die lêer is gesluit.",
+    "An internal server error occurred." : "’n Interne bedienerfout het voorgekom.",
+    "Cannot save file as it has been modified since opening" : "Lêer kan nie bewaar word nie want dit is gewysig sedert dit geopen is",
+    "Insufficient permissions" : "Onvoldoende toestemmings",
+    "File path not supplied" : "Lêerpad nie verskaf nie",
+    "File mtime not supplied" : "Lêer-mtyd nie verskaf nie",
+    "saving..." : "bewaar tans…",
+    "saved!" : "bewaar!",
+    "failed!" : "misluk!",
+    "Saved" : "Bewaar",
+    "There was a problem saving your changes. Click to resume editing." : "U veranderinge kon nie bewaar word nie. Klik om wysiging voort te sit.",
+    "An error occurred!" : "’n Fout het voorgekom!",
+    "Text file" : "Tekslêer",
+    "New text file.txt" : "Nuwe tekslêer.txt"
+},
+"nplurals=2; plural=(n != 1);");
