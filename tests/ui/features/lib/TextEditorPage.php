d5b3a1d1d3d533bf2d0c26fe8bc966a161529908
Phil Davis <phil@jankaritech.com>
2017-09-01 19:39:39 +0545

    Text editor UI tests (#204)
    * Text editor UI tests
    
    * sub-folders and file content checks
    
    * type in text area without using setValue
    
    * new text file tooltip checks
    
    * improve xpath selector for text lines
    
    * Improve waiting before closing editor

---
+++ tests/ui/features/lib/TextEditorPage.php
@@ -0,0 +1,234 @@
+<?php
+/**
+ * ownCloud
+ *
+ * @author Phillip Davis <phil@jankaritech.com>
+ * @copyright 2017 Phillip Davis phil@jankaritech.com
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace Page;
+
+use Behat\Mink\Session;
+use SensioLabs\Behat\PageObjectExtension\PageObject\Exception\ElementNotFoundException;
+use WebDriver\Key;
+
+/**
+ * Text Editor page.
+ */
+class TextEditorPage extends FilesPage {
+	protected $newTextFileButtonXpath
+		= './/div[contains(@class, "newFileMenu")]' .
+			'//a[@data-templatename="New text file.txt"]';
+	protected $newTextFileNameInputLabel = 'New text file.txt';
+	protected $newTextFileNameInputXpath
+		= './/div[contains(@class, "newFileMenu")]' .
+			'//a[@data-templatename="New text file.txt"]//input';
+	protected $newTextFileTooltipXpath = ".//*[@class='tooltip-inner']";
+	protected $textFileEditXpath = "//textarea[contains(@class,'ace_text-input')]";
+	protected $textFileTextLayerXpath = "//div[contains(@class,'ace_text-layer')]";
+	protected $textFileLineXpath = ".//div[@class='ace_line']";
+	protected $textEditorCloseButtonId = "editor_close";
+
+	/**
+	 * type in the field that matches the given xpath and optionally press enter.
+	 * Note: this depends on methods that might only be in the Selenium
+	 * implementation
+	 *
+	 * @param Session $session
+	 * @param string $xpath
+	 * @param string $text
+	 * @param bool $pressEnter
+	 * @throws ElementNotFoundException
+	 * @return void
+	 */
+	public function typeInField(
+		Session $session,
+		$xpath,
+		$text,
+		$pressEnter = false
+	) {
+		$element = $session->getDriver()->getWebDriverSession()->element(
+			"xpath", $xpath
+		);
+
+		if (is_null($element)) {
+			throw new ElementNotFoundException(
+				"could not find element with xpath '" . $xpath . "'"
+			);
+		}
+
+		$keys = preg_split('//u', $text, null, PREG_SPLIT_NO_EMPTY);
+		if ($pressEnter) {
+			$keys[] = Key::ENTER;
+		}
+		$element->postValue(array('value' => $keys));
+	}
+
+	/**
+	 * create a text file with the given name.
+	 * If name is not given the default is used.
+	 * If $useDefaultFileExtension is true, then only the name is entered and the
+	 * file extension is the default given by the application.
+	 *
+	 * @param Session $session
+	 * @param string $name
+	 * @param boolean $useDefaultFileExtension
+	 * @return void
+	 */
+	public function createTextFile(
+		Session $session,
+		$name = null,
+		$useDefaultFileExtension = false
+	) {
+		$newFileFolderButton
+			= $this->find("xpath", $this->newFileFolderButtonXpath);
+
+		if ($newFileFolderButton === null) {
+			throw new ElementNotFoundException(
+				"could not find new file/folder button"
+			);
+		}
+
+		$newFileFolderButton->click();
+
+		$newTextFileButton = $this->find("xpath", $this->newTextFileButtonXpath);
+
+		if ($newTextFileButton === null) {
+			throw new ElementNotFoundException(
+				"could not find new text file button"
+			);
+		}
+
+		$newTextFileButton->click();
+
+		if (strlen($name)) {
+			if ($useDefaultFileExtension) {
+				$this->typeInField(
+					$session,
+					$this->newTextFileNameInputXpath,
+					$name,
+					true
+				);
+			} else {
+				try {
+					$this->fillField($this->newTextFileNameInputLabel, $name . "\n");
+				} catch (\WebDriver\Exception\NoSuchElement $e) {
+					// this seems to be a bug in MinkSelenium2Driver.
+					// used to work fine in 1.3.1 but now throws this exception
+					// actually all that we need does happen,
+					// so we just don't do anything
+				}
+			}
+		} else {
+			$this->typeInField(
+				$session,
+				$this->newTextFileNameInputXpath,
+				'',
+				true
+			);
+		}
+
+		$this->waitForAjaxCallsToStartAndFinish($session);
+	}
+
+	/**
+	 * returns the tooltip that is displayed next to the new text file name box
+	 *
+	 * @return string
+	 */
+	public function getTooltipOfNewTextFileBox() {
+		$newTextFileTooltip = $this->find("xpath", $this->newTextFileTooltipXpath);
+
+		if ($newTextFileTooltip === null) {
+			throw new ElementNotFoundException(
+				"could not find new text file box tooltip"
+			);
+		}
+
+		return $newTextFileTooltip->getText();
+	}
+
+	/**
+	 * type text into the text area
+	 *
+	 * @param Session $session
+	 * @param string $text
+	 * @return void
+	 */
+	public function typeIntoTextFile(
+		Session $session,
+		$text
+	) {
+		$this->typeInField(
+			$session,
+			$this->textFileEditXpath,
+			$text
+		);
+	}
+
+	/**
+	 * get the content of the open text file
+	 *
+	 * @return array of lines of text
+	 */
+	public function textFileContent() {
+		$textLayer = $this->find("xpath", $this->textFileTextLayerXpath);
+		if ($textLayer === null) {
+			throw new ElementNotFoundException("could not find text layer");
+		}
+		$textLineElements = $textLayer->findAll(
+			"xpath", $this->textFileLineXpath
+		);
+		if ($textLineElements === null) {
+			throw new ElementNotFoundException("could not find text lines");
+		}
+		$textLines = [];
+		foreach ($textLineElements as $textLineElement) {
+			$textLines[] = $textLineElement->getText();
+		}
+		return $textLines;
+	}
+
+	/**
+	 *
+	 * @param Session $session
+	 * @throws ElementNotFoundException
+	 * @return void
+	 */
+	public function closeTheTextEditor(Session $session) {
+		$this->waitForAjaxCallsToStartAndFinish($session);
+
+		$closeButton = $this->findById($this->textEditorCloseButtonId);
+		if ($closeButton === null) {
+			throw new ElementNotFoundException(
+				"could not find text editor close button"
+			);
+		}
+		$closeButton->click();
+	}
+
+	/**
+	 * @param int $timeout_msec
+	 * @return void
+	 */
+	public function waitTillEditorIsLoaded(
+		$timeout_msec = STANDARDUIWAITTIMEOUTMILLISEC
+	) {
+		$this->waitTillElementIsNotNull($this->textFileEditXpath, $timeout_msec);
+	}
+
+}
---
+++ tests/ui/features/textfiles.feature
@@ -0,0 +1,117 @@
+Feature: textFiles
+
+	Background:
+		Given a regular user exists
+		And I am logged in as a regular user
+		And I am on the files page
+
+	Scenario Outline: Create a text file
+		When I create a text file with the name <file_name>
+		And I input <example_text> in the text area
+		And I close the text editor
+		Then the file <file_name> should be listed
+		And the files page is reloaded
+		Then the file <file_name> should be listed
+		And I open the file <file_name>
+		Then there is 1 line of text
+		And line 1 of the text is <example_text>
+		Examples:
+			|file_name               |example_text                      |
+			|'सिमप्ले text file.txt'    |'some text'                       |
+			|'"somequotes1" text.txt'|'John said,"hello."'              |
+			|"'somequotes2' text.txt"|'special !@#$%^&*()[]{}\|-_+=\/?' |
+
+	Scenario: Create a text file with the default name and file extension
+		When I create a text file with the name ""
+		And I input "stuff" in the text area
+		And I close the text editor
+		Then the file "New text file.txt" should be listed
+		And the files page is reloaded
+		Then the file "New text file.txt" should be listed
+
+	Scenario: Create a text file with the default file extension and do not close the editor
+		When I create a text file with the name "abc" without changing the default file extension
+		And I input "something" in the text area
+		Then the file "abc.txt" should be listed
+		And the files page is reloaded
+		Then the file "abc.txt" should be listed
+
+	Scenario: Create a text file with the default file extension and unicode file name
+		When I create a text file with the name "सिमप्ले text file" without changing the default file extension
+		And I input "नेपाल" in the text area
+		And I close the text editor
+		Then the file "सिमप्ले text file.txt" should be listed
+		And the files page is reloaded
+		Then the file "सिमप्ले text file.txt" should be listed
+		And I open the file "सिमप्ले text file.txt"
+		Then there is 1 line of text
+		And line 1 of the text is "नेपाल"
+
+	Scenario: Create a text file with multiple lines of text in it
+		When I create a text file with the name "atextfile.txt"
+		And I input the following text in the text area:
+		"""
+		What is this?
+		This is some "example" text!
+		That goes on some lines in a 'text' file.
+
+		नेपाल
+		1 2 3 4 5 6 7 8 9 0
+		"""
+		And I close the text editor
+		Then the file "atextfile.txt" should be listed
+		And the files page is reloaded
+		Then the file "atextfile.txt" should be listed
+		And I open the file "atextfile.txt"
+		Then there are 6 lines of text
+		And line 1 of the text is "What is this?"
+		And line 2 of the text is 'This is some "example" text!'
+		And line 3 of the text is "That goes on some lines in a 'text' file."
+		And line 4 of the text is ""
+		And line 5 of the text is "नेपाल"
+		And line 6 of the text is "1 2 3 4 5 6 7 8 9 0"
+
+	Scenario: Create a text file with the default name and file extension in a sub-folder
+		When I open the folder "simple-folder"
+		And I create a text file with the name ""
+		And I input "stuff" in the text area
+		And I close the text editor
+		Then the file "New text file.txt" should be listed
+		And the files page is reloaded
+		Then the file "New text file.txt" should be listed
+		And I open the file "New text file.txt"
+		Then line 1 of the text is "stuff"
+		And I input "other text before " in the text area
+		And I close the text editor
+		And I open the file "New text file.txt"
+		Then line 1 of the text is "other text before stuff"
+
+	Scenario: Create a text file in a sub-folder using special characters in the names
+		When I create a folder with the name "सिमप्ले फोल्देर $%#?&@"
+		And I open the folder "सिमप्ले फोल्देर $%#?&@"
+		And I create a text file with the name "सिमप्ले $%#?&@ name.txt"
+		And I input "a line of text" in the text area
+		And I close the text editor
+		Then the file "सिमप्ले $%#?&@ name.txt" should be listed
+		And the files page is reloaded
+		Then the file "सिमप्ले $%#?&@ name.txt" should be listed
+
+	Scenario: Create a text file putting a name of a file which already exists
+		When I create a text file with the name "lorem.txt"
+		Then near the new text file box a tooltip with the text 'lorem.txt already exists' should be displayed
+
+	Scenario: Create a text file named with forward slash
+		When I create a text file with the name "simple-folder/a.txt"
+		Then near the new text file box a tooltip with the text 'File name cannot contain "/".' should be displayed
+
+	Scenario: Create a text file named ..
+		When I create a text file with the name ".."
+		Then near the new text file box a tooltip with the text '".." is an invalid file name.' should be displayed
+
+	Scenario: Create a text file named .
+		When I create a text file with the name "."
+		Then near the new text file box a tooltip with the text '"." is an invalid file name.' should be displayed
+
+	Scenario: Create a text file with file extension .part
+		When I create a text file with the name "data.part"
+		Then near the new text file box a tooltip with the text '"data.part" has a forbidden file type/extension.' should be displayed
