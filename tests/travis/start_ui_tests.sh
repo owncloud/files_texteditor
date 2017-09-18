34f53f4a4910f80f7c4665938de6e82501f4c20a
Phil Davis <phil@jankaritech.com>
2017-09-18 12:21:31 +0545

    Rename core start_ui_tests script

--- tests/travis/start_ui_tests.sh
+++ tests/travis/start_ui_tests.sh
@@ -6,4 +6,4 @@
 # @copyright 2017 Phillip Davis phil@jankaritech.com
 #
 echo "Running UI tests"
-bash tests/travis/start_behat_tests.sh --config apps/files_texteditor/tests/ui/config/behat.yml
+bash tests/travis/start_ui_tests.sh --config apps/files_texteditor/tests/ui/config/behat.yml
