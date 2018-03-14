8e12162b28334ece56c43b27888d19d326604176
ownCloud Jenkins <jenkins@owncloud.com>
2018-03-14 17:02:27 +0000

    [tx-robot] updated from transifex

---
+++ l10n/nn_NO.js
@@ -0,0 +1,22 @@
+OC.L10N.register(
+    "files_texteditor",
+    {
+    "This file is too big to be opened. Please download the file instead." : "Fila er for stor for å opnast. Ver venleg å laste ned fila i staden for.",
+    "Cannot read the file." : "Kan ikkje lesa fila.",
+    "Invalid file path supplied." : "Ugyldig stig oppgjett.",
+    "The file is locked." : "Fila er låst.",
+    "An internal server error occurred." : "Ein intern feil oppstod!",
+    "Cannot save file as it has been modified since opening" : "Kan ikke lagra fila, denne er endra etter den vart opna",
+    "Insufficient permissions" : "Manglanda tilgjenge",
+    "File path not supplied" : "Område for fil er ikke gjeve",
+    "File mtime not supplied" : "mtime for fil er ikkje gjeve",
+    "saving..." : "lagrar...",
+    "saved!" : "lagra!",
+    "failed!" : "feila!",
+    "Saved" : "Lagra",
+    "There was a problem saving your changes. Click to resume editing." : "Feil ved lagring av endringar. Klikk for å fortsetje redigering. ",
+    "An error occurred!" : "Ein feil oppstod!",
+    "Text file" : "Tekst fil",
+    "New text file.txt" : "Ny tekst fil.txt"
+},
+"nplurals=2; plural=(n != 1);");
