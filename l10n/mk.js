b858c159a9bc46bbae1586369beb21e9a0d7dbfe
ownCloud Jenkins <jenkins@owncloud.com>
2018-05-28 02:04:25 +0000

    [tx-robot] updated from transifex

---
+++ l10n/mk.js
@@ -0,0 +1,22 @@
+OC.L10N.register(
+    "files_texteditor",
+    {
+    "This file is too big to be opened. Please download the file instead." : "Оваа датотека е премногу голема за да се отвори. Ве молиме симнете ја датотеката.",
+    "Cannot read the file." : "Не можае да се вчита датотеката.",
+    "Invalid file path supplied." : "Невалидна патека до датотека.",
+    "The file is locked." : "Датотеката е заклучена.",
+    "An internal server error occurred." : "Се случи внатрешна грешка на серверот.",
+    "Cannot save file as it has been modified since opening" : "Не може да се зачува датотеката бидејќи е модифицирана уште од отворањето",
+    "Insufficient permissions" : "Недоволни дозволи",
+    "File path not supplied" : "Патека до датотеката не испорачана",
+    "File mtime not supplied" : "Патека до датотеката не испорачана",
+    "saving..." : "зачувува...",
+    "saved!" : "зачувано!",
+    "failed!" : "не успеа!",
+    "Saved" : "Снимено",
+    "There was a problem saving your changes. Click to resume editing." : "Има проблем да ги зачувате вашите промени. Кликни за да продолжите со уредувањето.",
+    "An error occurred!" : "Се случи грешка",
+    "Text file" : "Текстуална датотека",
+    "New text file.txt" : "Нова текстуална датотека file.txt"
+},
+"nplurals=2; plural=(n % 10 == 1 && n % 100 != 11) ? 0 : 1;");
