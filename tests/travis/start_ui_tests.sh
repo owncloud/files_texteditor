00dd0d9592715422d39e9467b1b91ab7a18c3f2c
Phil Davis <phil@jankaritech.com>
2018-07-26 14:10:49 +0545

    Use run.sh for webUI tests

--- tests/travis/start_ui_tests.sh
+++ tests/travis/start_ui_tests.sh
@@ -7,4 +7,8 @@
 #
 echo "Running UI tests"
 export APPS_TO_ENABLE="files_texteditor"
-bash tests/travis/start_ui_tests.sh --config apps/files_texteditor/tests/acceptance/config/behat.yml
+pushd tests/acceptance
+./run.sh --config ../../apps/files_texteditor/tests/acceptance/config/behat.yml --suite webUITextEditor
+ACCEPTANCE_TEST_EXIT_STATUS=$?
+popd
+exit $ACCEPTANCE_TEST_EXIT_STATUS
