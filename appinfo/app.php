4aa772b33cda5ff8859c9495851a2c493b270b0a
Dipak Acharya <dpakach@gmail.com>
2018-11-19 10:28:22 +0545

    Fix code style and add owncloud coding standard in drone

--- appinfo/app.php
+++ appinfo/app.php
@@ -3,7 +3,7 @@
 // only load text editor if the user is logged in
 if (\OCP\User::isLoggedIn()) {
 	$eventDispatcher = \OC::$server->getEventDispatcher();
-	$eventDispatcher->addListener('OCA\Files::loadAdditionalScripts', function() {
+	$eventDispatcher->addListener('OCA\Files::loadAdditionalScripts', function () {
 		OCP\Util::addStyle('files_texteditor', 'DroidSansMono/stylesheet');
 		OCP\Util::addStyle('files_texteditor', 'style');
 		OCP\Util::addStyle('files_texteditor', 'mobile');
@@ -11,4 +11,3 @@ if (\OCP\User::isLoggedIn()) {
 		OCP\Util::addscript('files_texteditor', 'vendor/ace/src-noconflict/ace');
 	});
 }
-
