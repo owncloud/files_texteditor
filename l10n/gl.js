2f9c2075346d3e382602f78871097c0fe1101367
ownClouders <devops@owncloud.com>
2019-01-19 05:04:29 +0000

    [tx] updated from transifex

--- l10n/gl.js
+++ l10n/gl.js
@@ -1,18 +1,22 @@
 OC.L10N.register(
     "files_texteditor",
     {
+    "This file is too big to be opened. Please download the file instead." : "O ficheiro é grande de máis para ser aberto. Descargue o ficheiro no canto de abrilo.",
+    "Cannot read the file." : "Non é posíbel ler o ficheiro.",
     "Invalid file path supplied." : "Forneceu unha ruta incorrecta.",
+    "The file is locked." : "O ficheiro está bloqueado.",
+    "An internal server error occurred." : "Produciuse un erro interno no servidor.",
     "Cannot save file as it has been modified since opening" : "Non é posíbel gardar o ficheiro, xa que foi modificado trala súa apertura",
     "Insufficient permissions" : "Non ten permisos abondo",
     "File path not supplied" : "Non forneceu unha ruta de ficheiro",
     "File mtime not supplied" : "Non forneceu o ficheiro mtime",
-    "Save" : "Gardar",
-    "Search" : "Buscar",
-    "Next" : "Seguinte",
-    "Clear" : "Limpar",
-    "Saving..." : "Gardando...",
+    "saving..." : "gardando...",
+    "saved!" : "gardado!",
+    "failed!" : "fallou!",
+    "Saved" : "Gardado",
+    "There was a problem saving your changes. Click to resume editing." : "Houbo un problema ao gardar os cambios. Prema para continuar a edición.",
     "An error occurred!" : "Produciuse un erro!",
-    "There are unsaved changes in the text editor" : "Hai cambios sen gardar no editor de textos",
-    "There were unsaved changes, click here to go back" : "Hai cambios sen gardar, prema aquí para ir cara atrás"
+    "Text file" : "Ficheiro de texto",
+    "New text file.txt" : "Novo ficheiro de texto.txt"
 },
 "nplurals=2; plural=(n != 1);");
