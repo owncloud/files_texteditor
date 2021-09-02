222de02b0e6d86b2c00aa46b3815d9ce68fd1168
Michael Barz <mbarz@owncloud.com>
2021-09-02 00:05:42 +0000

    [tx] updated from transifex

--- l10n/sq.js
+++ l10n/sq.js
@@ -1,6 +1,8 @@
 OC.L10N.register(
     "files_texteditor",
     {
+    "Invalid share token" : "Token i pavlefshëm ndarjesh",
+    "No user found" : "S’u gjet përdorues",
     "This file is too big to be opened. Please download the file instead." : "Kjo kartelë është shumë e madhe për hapje. Ju lutemi, në vend të kësaj, shkarkojeni.",
     "Cannot read the file." : "S’lexohet dot kartela.",
     "Invalid file path supplied." : "U dha shteg i pavlefshëm.",
