cc938fe8d5061cd61160fdf3beda5396538c75e5
ownClouders <devops@owncloud.com>
2022-02-06 00:20:33 +0000

    [tx] updated from transifex

--- l10n/th_TH.js
+++ l10n/th_TH.js
@@ -1,14 +1,16 @@
 OC.L10N.register(
     "files_texteditor",
     {
+    "Invalid share token" : "โทเค็นการแชร์ไม่ถูกต้อง",
+    "No user found" : "ไม่พบผู้ใช้",
     "This file is too big to be opened. Please download the file instead." : "เปิดไฟล์นี้ไม่ได้ เพราะมีขนาดใหญ่เกินไป กรุณาดาวน์โหลดไฟล์แทน",
     "Cannot read the file." : "ไม่สามารถอ่านไฟล์",
-    "Invalid file path supplied." : "เส้นทางของไฟล์ที่ให้มาไม่ถูกต้อง",
+    "Invalid file path supplied." : "Path ของไฟล์ไม่ถูกต้อง",
     "The file is locked." : "ไฟล์ถูกล็อค",
     "An internal server error occurred." : "มีข้อผิดพลาดเกิดขึ้นภายในเซิร์ฟเวอร์",
     "Cannot save file as it has been modified since opening" : "ไม่สามารถบันทึกไฟล์ในขณะที่มันถูกเปิดและแก้ไขอยู่",
     "Insufficient permissions" : "สิทธิ์ไม่เพียงพอ",
-    "File path not supplied" : "ไม่ได้ระบุเส้นทางของไฟล์",
+    "File path not supplied" : "ไม่ได้ระบุ path ของไฟล์",
     "File mtime not supplied" : "ไม่ได้ให้ไฟล์ mtime มาด้วย",
     "saving..." : "กำลังบันทึก...",
     "saved!" : "บันทึกแล้ว!",
